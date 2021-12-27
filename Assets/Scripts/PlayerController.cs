4b33fd0aa23e2758d3c75899407e08ca0f770db8
Kayla Brewer <kbrewer@cap-rx.com>
2021-12-27 17:43:36 -0600

    better movement, some collision

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,29 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
+
+    public CharacterController charCon;
+
+    private CameraController camCon;
+
+    private Vector3 moveAmount;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        camCon = FindObjectOfType<CameraController>();
+        charCon = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(transform.position.x + Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, transform.position.y, transform.position.z + Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
+        moveAmount = (camCon.transform.forward * Input.GetAxis("Vertical")) + (camCon.transform.right * Input.GetAxis("Horizontal"));
+
+        moveAmount.y = 0f;
+
+        moveAmount = moveAmount.normalized;
+
+        charCon.Move(moveAmount * moveSpeed * Time.deltaTime);
     }
 }
